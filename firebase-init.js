586c798e52c35c441ac18faa7d16f980b3fdd2f3
Ana <anaaaaaaajks@gmail.com>
2025-11-07 17:35:38 +0000

    b

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDpGW74IAinvKv_NnYOlrUIr7XHtsm3wL0",
+  authDomain: "sitenickson.firebaseapp.com",
+  projectId: "sitenickson",
+  storageBucket: "sitenickson.firebasestorage.app",
+  messagingSenderId: "599304203382",
+  appId: "1:599304203382:web:c4e1d5d5400bd14547c357",
+  measurementId: "G-VBCNMH0100"
+};
+firebase.initializeApp(firebaseConfig); 
+  
