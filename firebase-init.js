bb49b5d8f868745848135d876e28ea55143938e5
eduarda1606 <eduardarodriguesrr06@gmail.com>
2025-12-05 18:22:59 +0000

    g

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDpGW74IAinvKv_NnYOlrUIr7XHtsm3wL0",
-  authDomain: "sitenickson.firebaseapp.com",
-  projectId: "sitenickson",
-  storageBucket: "sitenickson.firebasestorage.app",
-  messagingSenderId: "599304203382",
-  appId: "1:599304203382:web:c4e1d5d5400bd14547c357",
-  measurementId: "G-VBCNMH0100"
-};
+    apiKey: "AIzaSyA3TPCNAfna5s95AVpEhK81rRV5fbrI7ZE",
+    authDomain: "projeto-nickson.firebaseapp.com",
+    projectId: "projeto-nickson",
+    storageBucket: "projeto-nickson.firebasestorage.app",
+    messagingSenderId: "290357639982",
+    appId: "1:290357639982:web:3a19d35791e935de73d8bf",
+    measurementId: "G-B6LV3J6QY6"
+  };
 firebase.initializeApp(firebaseConfig); 
   
